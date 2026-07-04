57d10b705d8097cd13d1fd485b2fcedd8dcf13bb
AlejandroFranco1101 <alejandrofranco1101@gmail.com>
2026-07-04 14:55:53 -0600

    Lógica de la aplicación
    Integrando logica de funsion en los botones

--- app/src/main/java/com/example/guia2_ejercicio1/MainActivity.kt
+++ app/src/main/java/com/example/guia2_ejercicio1/MainActivity.kt
@@ -1,20 +1,63 @@
 package com.example.guia2_ejercicio1
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-
 class MainActivity : AppCompatActivity() {
+// 1. Variables
+    lateinit var btnSaludar: Button
+    lateinit var btnLimpiar: Button
+    lateinit var etNombre: EditText
+    lateinit var tvSaludo: TextView
+    lateinit var tvMensaje: TextView
+// 2. Metodo del ciclo de vida del activity
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        // 3. Establecer archivo layout
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        // 4. Referencias a elementos de la UI
+        btnSaludar = findViewById(R.id.btnSaludar)
+        btnLimpiar = findViewById(R.id.btnLimpiar)
+        etNombre = findViewById(R.id.etNombre)
+        tvSaludo = findViewById(R.id.tvSaludo)
+        tvMensaje = findViewById(R.id.tvMensaje)
+        tvSaludo.text = ""
+        tvMensaje.text = ""
+    // 5. Listeners de los botones
+        btnSaludar.setOnClickListener {
+            val nombre = etNombre.text.toString().trim()
+            if (nombre.isEmpty()) {
+                mostrarToast("Error, el campo nombre esta vacio.")
+            }
+            else {
+                val saludo = "Hola, $nombre"
+                val mensaje = "Bienvenido al laboratorio 2 de DSM441."
+                tvSaludo.text = saludo
+                tvMensaje.text = mensaje
+                mostrarToast("Saludo generado exitosamente")
+            }
+    }
+    btnLimpiar.setOnClickListener {
+        etNombre.text.clear()
+        tvSaludo.text = ""
+        tvMensaje.text = ""
+        etNombre.requestFocus()
+        mostrarToast("Pantalla reiniciada")}
+}
+    // Funcion auxiliar para mostrar mensaje emergente
+    private fun mostrarToast(
+        mensaje: String,
+        duracion: Int = Toast.LENGTH_SHORT
+    ) {
+        Toast.makeText(
+            this,
+            mensaje,
+            duracion
+        ).show()
     }
 }
+
